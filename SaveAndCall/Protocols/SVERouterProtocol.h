14bd295f14cb088436eefaa0049ad37271677caf
Влад Снигирев <vladsnigirev@Vlads-MacBook-Pro.local>
2018-02-20 14:42:22 +0300

    1.0 Ver

---
+++ SaveAndCall/Protocols/SVERouterProtocol.h
@@ -0,0 +1,16 @@
+//
+//  SVERouterProtocol.h
+//  SaveAndCall
+//
+//  Created by Влад Снигирев on 12/02/2018.
+//  Copyright © 2018 Vlad Snigiryov. All rights reserved.
+//
+
+
+//Протокол для смены экранов
+@protocol SVERouterProtocol <NSObject>
+
+- (void)switchAuthorizationControllerToMain;
+- (void)switchMainControllerToAuthorization;
+
+@end
